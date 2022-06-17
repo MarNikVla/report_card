2f06b8b97c64859f65314fc4fef5eb732c3cb7ad
marnikvla <marnikvla@yandex.ru>
2022-06-17 22:54:46 +0300

    add Counter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook
+from collections import  Counter
 from random import randint
 
 wb = load_workbook(filename='test.xlsx')
@@ -9,8 +10,8 @@ machinist_sheet, DEM_sheet, reason_of_absence_sheet = wb[machinist_sheet_name], 
                                                       wb[reason_sheet_name]
 line_of_workers = dict()
 
-for i in machinist_sheet['b24:b25']:
-    print(i[0].value)
+# for i in machinist_sheet['b24:b25']:
+    # print(i[0].value)
 
 for col in DEM_sheet.iter_cols(min_row=13, max_row=49, min_col=2, max_col=2):
     for cell in col:
@@ -19,4 +20,48 @@ for col in DEM_sheet.iter_cols(min_row=13, max_row=49, min_col=2, max_col=2):
             line_of_workers[cell.value] = [cell.offset(row=i, column=j).value for i in [0, 1] for j
                                            in range(1, 17)]
 
-print(*line_of_workers.items(), sep='\n')
+attendance_days = 0
+absence_days = 0
+vacation_days = 0
+medical_days = 0
+other_absence_days = 0
+hours = 0
+night_hours = 0
+
+def cont_attendance_days(lst:list):
+    c= Counter(lst)
+    print(c)
+
+
+
+def intenize_from_list(lst: list):
+    new_lst = list()
+    for string in lst:
+        try:
+            new_lst.append(float(string.replace(",", ".")))
+        except ValueError:
+            new_lst.append(string)
+    print(new_lst)
+    return new_lst
+
+attend_days = 0
+absence_days = 0
+vacation_days = 0
+medical_days = 0
+other_absence_days = 0
+hours = 0
+night_hours = 0
+
+def cont_days(intenize_list:list):
+    for item in intenize_list:
+        attend_days = item.count
+    pass
+
+
+
+
+if __name__ == '__main__':
+    # intenize_from_list(line_of_workers['Буржинский А.В. Эл.монтер ЩУ ГТУ'])
+    intenize_from_list(line_of_workers['Канева М.А. Уборщица '])
+    cont_attendance_days(line_of_workers['Канева М.А. Уборщица '])
+    cont_attendance_days(line_of_workers['Буржинский А.В. Эл.монтер ЩУ ГТУ'])
